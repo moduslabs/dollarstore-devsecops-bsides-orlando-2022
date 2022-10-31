9269fee45585fcb42c6dc263dafc1aebabd866d5
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2022-10-31 08:30:01 -0400

    Add extra config options to Jenkins server TF

--- terraform/network-infra/modules/jenkins_server.tf
+++ terraform/network-infra/modules/jenkins_server.tf
@@ -14,11 +14,18 @@ resource "aws_instance" "jenkins_main" {
     key_name = aws_key_pair.jenkins_key.key_name
     vpc_security_group_ids = [aws_security_group.jenkins_main_sg.id]
     subnet_id = element(aws_subnet.private_subnets, 0).id
+    monitoring = true
+    ebs_optimized = true
     root_block_device { 
         volume_type = "gp3" 
         volume_size = 30 
         delete_on_termination = false 
     }
+    metadata_options {
+        http_endpoint = "enabled"
+        http_tokens   = "required"
+    }
+
 
     tags = {
     	Name = "Jenkins Server Main"

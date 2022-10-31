6afb93bd833de2c06379bd4c3c1a28dc68596fa7
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2022-10-31 09:13:22 -0400

    Add descriptions to SG

--- terraform/network-infra/modules/jenkins_sg.tf
+++ terraform/network-infra/modules/jenkins_sg.tf
@@ -3,15 +3,17 @@ resource "aws_security_group" "elb" {
     vpc_id = var.vpc_id 
     
     ingress { 
-    	protocol    = "tcp" 
+        protocol    = "tcp" 
+        description = "Access on port 80 to Jenkins"
         from_port   = 80 
         to_port     = 80 
         cidr_blocks = ["0.0.0.0/0"] 
      } 
      egress { 
-     	from_port   = 0 
+        from_port   = 0
         to_port     = 0 
-        protocol    = "-1" 
+        protocol    = "-1"
+        description = "Allow outbound traffic" 
         cidr_blocks = ["0.0.0.0/0"] 
      } 
 

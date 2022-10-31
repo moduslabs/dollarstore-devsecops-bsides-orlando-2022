e1ca419e93d9ba941946cecee8e13683921ff80e
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2022-10-31 08:08:06 -0400

    Add extra config to bastion host via teeraform

--- terraform/network-infra/modules/bastion_host.tf
+++ terraform/network-infra/modules/bastion_host.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "bastion_host_sg" {
         protocol  = "tcp"
         from_port = 22
         to_port  = 22
-        cidr_blocks = ["0.0.0.0/0"] /*change to your IP after deployment via the console*/
+        cidr_blocks = ["50.197.6.145/32"] /*change to your IP after deployment via the console*/
     }
 
     egress {
@@ -32,13 +32,21 @@ data "aws_ssm_parameter" "linux2_ami" {
 }
 
 resource "aws_instance" "bastion_host_server" { 
+    #checkov:skip=CKV_AWS_88:Public IP for connection to bastion host
     ami = data.aws_ssm_parameter.linux2_ami.value 
     key_name  = aws_key_pair.bastion_host_key.key_name
     instance_type = "t3.nano"
     associate_public_ip_address = true
     subnet_id = element(aws_subnet.public_subnets, 0).id
     vpc_security_group_ids = [aws_security_group.bastion_host_sg.id]
-    
+    monitoring = true
+    ebs_optimized = true
+
+    metadata_options {
+        http_endpoint = "enabled"
+        http_tokens   = "required"
+    }
+
     tags = {
     	Name = "jenkins-bastion-host" 
     }

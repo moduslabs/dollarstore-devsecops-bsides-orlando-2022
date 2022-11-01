77d14080620445247843855a29b58c5ad4b84e99
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2022-11-01 09:25:44 -0400

    Add Docker and Git support to packer setup script

--- packer/setup.sh
+++ packer/setup.sh
@@ -5,8 +5,14 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade
 sudo amazon-linux-extras install java-openjdk11 -y
-
+sudo yum install git -y
+sudo yum install docker -y
+sudo usermod -a -G docker ec2-user
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
 sudo yum install jenkins -y
+sudo usermod -a -G docker jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins

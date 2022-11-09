2c3a1129434c035e418fb73b1c91d61ef27f7e00
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2022-11-09 08:11:40 -0500

    Add some more example JS code

--- js/get_users.js
+++ js/get_users.js
@@ -1,14 +1,18 @@
 /* Example JS code for security scans */
 
-function evalIsBad() {
+const express = require('express');
+const apiDb = require('./db');
+const router = express.Router();
 
-    eval(new String('1 + 2'))
+router.get('/users', (req, res) => {
+  db.query('SELECT user_email FROM users WHERE id = ' + req.query.id);
+    .then((record) => {
+      res.send(record[0]);
+      debugger(record[0]);
+    })
+});
+
+function debugger(record) {
+    eval(new String(record))
 }
-
-
-
-
-
-
-evalIsBad();
 
